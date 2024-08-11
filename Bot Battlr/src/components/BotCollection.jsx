6d27d823a0305c222cc4f4e8fe2c39c82548f587
hannington owiti <hanningtonowiti@gmail.com>
2024-08-11 14:48:31 +0300

    BotCollection td

--- Bot Battlr/src/components/BotCollection.jsx
+++ Bot Battlr/src/components/BotCollection.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-function BotCollection() {
-  return (
-    <div>BotCollection</div>
-  )
-}
+const BotCollection = ({}) => (
+    <tr>
+    <td>{bot.id}</td>
+    <td>{bot.name}</td>
+    <td>{bot.health}</td>
+    <td>{bot.damage}</td>
+    <td>{bot.armor}</td>
+    <td>{bot.bot_class}</td>
+    <td>
+    <img
+    src={bot.avatar_url}
+    alt={bot.name}
+    style={{width: '100px', height: 'auto'}}
+    onError={(e) => e.target.src = 'https://via.placeholder.com/100'}
+    />
+    </td>
+    <td>
+        <button>X</button>
+    </td>
+    </tr>
+);
+// {
+//   return (
+//     <div>BotCollection</div>
+//   )
+// }
 
 export default BotCollection
