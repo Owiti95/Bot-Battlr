87cf32e385fbd9924bb290d6aebee9e9e7598fbe
hannington owiti <hanningtonowiti@gmail.com>
2024-08-11 15:17:00 +0300

    YourBotArmy

--- Bot Battlr/src/components/YourBotArmy.jsx
+++ Bot Battlr/src/components/YourBotArmy.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-function YourBotArmy() {
-  return (
-    <div>YourBotArmy</div>
-  )
+const YourBotArmy = () => {
+    <tr>
+        <td>{bot.id}</td>
+        <td>{bot.name}</td>
+        <td>{bot.health}</td>
+        <td>bot.damage</td>
+        <td>bot.armor</td>
+        <td>
+            <img
+            src={bot.avatar_url}
+            alt={bot.name}
+            style={{width: '95px', height: 'auto' }}
+            onError={(e) => e.target.src = 'https://via.placeholder.com/100'}
+            />
+        </td>
+        <td>
+            <button>Remove</button>
+        </td>
+    </tr>
 }
 
 export default YourBotArmy
